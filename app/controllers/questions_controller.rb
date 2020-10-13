cd7607df5d70e883bb6153d302030c87d144c0dc
Tay Yang Wong <wongtakkyan@gmail.com>
2020-10-13 21:53:27 +0200

    fixed some stuff

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,11 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question.end_with? '?'
-      @answer = 'Silly question, get dressed and go to work!'
-    elsif @question == 'I am going to work right now!'
+
+    if @question == 'I am going to work right now!'
       @answer = 'Great!'
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
     else @answer = "I don't care, get dressed and go to work!"
     end
   end
